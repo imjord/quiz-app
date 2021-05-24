eccdbdeb735f5ee4310a0658ba7ab4a848e254c4
imjord <dean.jay.goat@gmail.com>
2021-05-23 19:22:26 -0600

    initial stuff

---
+++ script.js
@@ -0,0 +1,35 @@
+const quizData = [{
+    
+        question: 'what does html stand for?',
+        a: 'Hyper toggle makeup language',
+        b: 'hypertext markup language',
+        c: 'hotdogs to make laugh',
+        d: 'Cascading style sheet',
+        correct: 'b'
+    }, 
+    {
+        question: 'what is the most used programming language',
+        a: 'python',
+        b: 'java',
+        c: 'c+',
+        d: 'javascript',
+        correct: 'd'
+    }, 
+    {
+        question: 'who is the president of the usa',
+        a: 'obama',
+        b: 'donald trump',
+        c: 'joe biden',
+        d: 'george bush',
+        correct: 'c'
+
+    },
+    {
+        question: 'what year was javascript launched',
+        a: '1996',
+        b: '1995',
+        c: '2018',
+        d: 'none of the above',
+        correct: 'd'
+    }
+];

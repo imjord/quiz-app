1988886f1f69cc95f525032dd1fd480cb8f0cf36
imjord <dean.jay.goat@gmail.com>
2021-05-24 16:31:19 -0600

    final commit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// object var to hold question data.
+
 const quizData = [{
     
         question: 'what does html stand for?',
@@ -34,21 +36,39 @@ const quizData = [{
     }
 ];
 
+// vars to get elements needed from html.
+
+const quiz = document.getElementById('quiz');
 const questionEl = document.getElementById("question");
+const answerEls = document.querySelectorAll(".answer");
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
 const submitBtn = document.getElementById("submit");
 
+// var for score and current page.
 
-var currentQuiz = 0;
+let currentQuiz = 0;
+let score = 0;
 
 
+
+// function to start
 
 loadQuiz();
 
+
+
+
+
+
+
+
 function loadQuiz() {
+    deselectAnswers();
+
+
     const currentQuizData = quizData[currentQuiz];
    
    
@@ -63,17 +83,46 @@ function loadQuiz() {
    
 }
 
+function getSelected() {
+
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+        
+    });
+    
+   return answer;
+}
+
+
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
 
 submitBtn.addEventListener("click", function() {
-    currentQuiz++
 
-    if(currentQuiz < quizData.length) {
-        loadQuiz();
-    } else {
+    const answer = getSelected();
+    if(answer) {
+        if(answer === quizData[currentQuiz].correct) {
+            score++;
+        }
 
-        // show results 
-        alert('test is over!');
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `
+            <h2> you answered correctly at ${score}/${quizData.length} questions. </h2>
+            
+            <button onclick="location.reload()">Reload<button>`;
+
+        }
     }
-
     
 });
